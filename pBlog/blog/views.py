eeea6cd4bff807a0b22527a53c9a8c2c3f4711b4
Joao Guedes <jdihzy@gmail.com>
2021-07-13 17:16:32 +0900

    heroku test

--- pBlog/blog/views.py
+++ pBlog/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
-IndexView(request):
+def IndexView(request):
     return HttpResponse('This is a test')
     
