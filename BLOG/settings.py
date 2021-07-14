ae297cca9df87ae618618f4b4f74b520070629a7
Joao Guedes <jdihzy@gmail.com>
2021-07-14 21:44:51 +0900

    initial

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'BLOG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'BLOG/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
