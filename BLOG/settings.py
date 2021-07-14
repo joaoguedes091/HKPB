e13f99637f0eafffd50999c96d50aa8487225337
Joao Guedes <jdihzy@gmail.com>
2021-07-14 19:11:19 +0900

    initial

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'BLOG/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'BLOG/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
