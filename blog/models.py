80e9e674b2031113432e765ae72de44db44c5175
Joao Guedes <jdihzy@gmail.com>
2021-07-16 09:14:46 +0900

    initial

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class Test(models.Model):
     name = models.CharField(max_length=254)
+
+
+
+'''class BlogPosts(models.Model):
+    date = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.CharField(max_length=100)
+
+
+class Projects(models.Model):
+    date = models.CharField(max_length=100)
+    project_name = models.CharField(max_length=254)
+    description = models.TextField()'''
+    #filefield right here to insert a cool preview pic of the project
+
+
